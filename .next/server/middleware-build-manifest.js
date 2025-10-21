c3e2cded471a386a791136b38342d810d5e90478
Tom Murphy <tom.murphy@me.com>
2025-10-21 15:36:05 +0000

    Critical Thinking articles uploaded

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -25,6 +25,11 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
+    "/articles": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/articles.js"
+    ],
     "/downloads": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
