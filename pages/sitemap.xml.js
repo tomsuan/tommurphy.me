6d15ae1f6101b49e69778f84d0ff7c1ea3f5f4f0
Tom Murphy <tom.murphy@me.com>
2026-03-02 14:03:46 +0000

    Add static sitemap.xml with all pages and articles

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,73 @@
+import { getAllPostSlugs } from "../lib/content";
+
+const SITE_URL = "https://tommurphy.me";
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${SITE_URL}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/articles</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/videos</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/photos</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/downloads</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>${SITE_URL}/about</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  ${posts
+    .map(({ slug }) => {
+      return `
+  <url>
+    <loc>${`${SITE_URL}/articles/${slug}`}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+    })
+    .join("")}
+</urlset>`;
+}
+
+export async function getServerSideProps({ res }) {
+  const slugs = getAllPostSlugs();
+  const posts = slugs.map((slug) => ({ slug }));
+
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader("Content-Type", "application/xml");
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
+}
+
+export default function Sitemap() {
+  return null;
+}
